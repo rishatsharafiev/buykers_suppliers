13843d9111773afb9b9d1d66da9caf780d0dcd8c
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-10-11 12:43:15 +0300

    Remove picture_field, change __str__ method for Good model

--- apps/bpc/models/good.py
+++ apps/bpc/models/good.py
@@ -30,4 +30,4 @@ class Good(models.Model):
         verbose_name_plural = 'Товары'
 
     def __str__(self):
-        return self.code
+        return f'{self.code} {self.vendor_code} {self.nomenclature}'
