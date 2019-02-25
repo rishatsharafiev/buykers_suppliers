d0af3ddadc8129d779e7883ce8088f168c798d09
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-02-25 13:21:47 +0300

    Remove category_info admin

--- apps/fcmoto/admin/category_info.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-class CategoryInfoAdmin(admin.ModelAdmin):
-    """CategoryInfo admin"""
-
-    list_display = ('id', 'name',)
-    search_fields = ('name',)
-    list_per_page = 20
